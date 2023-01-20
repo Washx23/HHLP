641505e429e1d6ef74a77dc5bd5ea2ef4faea79d
washx23 <5731@holbertonstudents.com>
2023-01-20 06:08:05 -0800

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 import sys 
 if __name__ == '__main__':
-    print("{} arguments:".format(len(sys.argv)-1))
-    for args in range(len(sys.argv)):
-        count = int(args)
-        if count != 0:
-            print("{}: {}".format(count, str(sys.argv[args])))
+    if len(sys.argv) < 2:
+        print("{} argument:".format(len(sys.argv)-1))
+    elif len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv)-1))       
+    else:
+        print("{} arguments:".format(len(sys.argv)-1))
+        for args in range(len(sys.argv)):
+            count = int(args)
+            if count != 0:
+                print("{}: {}".format(count, str(sys.argv[args])))
