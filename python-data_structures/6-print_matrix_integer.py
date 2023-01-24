cef1868332be8e0ddda6361b6e6a8156a3a27def
washx23 <5731@holbertonstudents.com>
2023-01-24 10:10:02 -0800

    6-print_matrix_integer.py

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for a in matrix:
+        for b in a:
+            print("{:d}".format(b), end="")
+            if b != a:
+                print(" ", end="")
+        print("")
