9046c3985eede460a053af4c2fb351a6b45fc008
washx23 <5731@holbertonstudents.com>
2023-01-25 05:43:27 -0800

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
     while len(tuple_b) < 2:
         apl = list(tuple_b)
         apl.append(0)
@@ -20,5 +19,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         cp = tuple(cp[:2])
         recu = add_tuple(apl, cp)
         return recu
-    new_tuple = tuple([a + b for a, b in (zip(tuple_a, tuple_b))])    
+    new_tuple = tuple([a + b for a, b in (zip(tuple_a, tuple_b))])
     return new_tuple
