844081620ef947f000f1cff463ea3db6645ed2ef
washx23 <5731@holbertonstudents.com>
2023-01-29 11:35:51 -0800

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        return max(a_dictionary)
-    else:
-        return None
+    return max(a_dictionary, key=lambda x: a_dictionary[x]) if a_dictionary else None
