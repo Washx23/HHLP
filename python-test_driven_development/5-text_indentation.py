be5ce679d8e1c0b5f3745014b545bc24ac7e9f0b
washx23 <5731@holbertonstudents.com>
2023-02-04 14:03:03 -0800

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+function that prints a text with 2 new lines after each of these characters: ., ? and :
+"""
 
 def text_indentation(text):
+    """ function"""
     if type(text)==str:
         text = text.replace("?", "?\n\n")
         text = text.replace(".", ".\n\n")
