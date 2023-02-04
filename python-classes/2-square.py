3822a37bc46ecd88bcf4f4ce725da5fb35c85c0e
washx23 <5731@holbertonstudents.com>
2023-02-04 15:35:33 -0800

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, __size= 0):
+        if __size < 0:
+            raise ValueError("size must be >= 0")
+        if type(__size) != int:
+            raise TypeError("size must be an integer") 
+        self.__size = __size
