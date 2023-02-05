8990e2c2ebcb1d53f6e81dc824312d18bd3f75ef
washx23 <5731@holbertonstudents.com>
2023-02-04 20:31:00 -0800

    fix pep8

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -4,28 +4,32 @@
 
 class Square:
     """Square"""
-    def __init__(self, __size= 0):
+    def __init__(self, __size=0):
         """Square"""
         if __size < 0:
             raise ValueError("size must be >= 0")
         if type(__size) is not int:
-            raise TypeError("size must be an integer") 
+            raise TypeError("size must be an integer")
         self.__size = __size
+
     @property
     def size(self):
         """size"""
         return self.__size
+
     @size.setter
     def size(self, var):
         """size"""
         if int(var) < 0:
             raise ValueError("size must be >= 0")
         if type(var) is not int:
-            raise TypeError("size must be an integer") 
+            raise TypeError("size must be an integer")
         self.__size = var
+
     def area(self):
         """area"""
         return self.__size * self.__size
+
     def my_print(self):
         """my_print"""
         if self.__size == 0:
