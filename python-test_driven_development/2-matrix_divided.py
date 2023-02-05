5b48ce298d35e4354c06d26728ba2c65ee83532e
washx23 <5731@holbertonstudents.com>
2023-02-05 13:11:48 -0800

    2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""Matrix_divided"""
+"""2-matrix_divided"""
 
 
 def matrix_divided(matrix, div):
-    """matrix_divided"""
-    if type(div) != int and type(div) != float:
+    """matrix"""
+    row = len(matrix)
+    row_size = len(matrix[0])
+    new_matrix = [[0 for col in range(row_size)] for row in range(row)]
+
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
-        raise TypeError("division by zero")
-    new_matrix = []
-    for elements in matrix:
-        if type(elements) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of " +
-                            "integers/floats")
-        new_list = []
-        for i in elements:
-            if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of " +
-                                "integers/floats")
-            x = i / div
-            new_list.append(round(x, 2))
-        new_matrix.append(new_list)
-    listt = len(matrix[0])
-    for r in matrix:
-        if len(r) != listt:
+        raise ZeroDivisionError("division by zero")
+
+    for i in range(row):
+        if len(matrix[i]) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
-    return new_matrix
+
+    for i, item in enumerate(matrix):
+        for j, aux in enumerate(item):
+            if type(aux) is not int and type(aux) is not float:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) "
+                    "of integers/floats")
+
+            new_matrix[i][j] = round((aux / div), 2)
+    return (new_matrix)
