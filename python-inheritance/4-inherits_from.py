7f4b8502f12527472dcaa0898988ef90ee93dccd
washx23 <5731@holbertonstudents.com>
2023-02-08 08:04:22 -0800

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,5 +4,6 @@
 
 def inherits_from(obj, a_class):
     """ inherits_rom """
-    if obj is not float or obj is not None:
-        return type(obj) is not a_class
+    if obj is None and a_class is object:
+        return False 
+    return type(obj) is not a_class
