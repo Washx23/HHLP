31cc2c0af63346a6db509bb1f1c7e9f95c05317e
washx23 <5731@holbertonstudents.com>
2023-02-08 07:45:54 -0800

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,6 +2,6 @@
 """4-inherits_from"""
 
 
-def is_kind_of_class(obj, a_class):
-    """is_kind_of_class"""
-    return isinstance(obj, a_class)
+def inherits_from(obj, a_class):
+    """ inherits_rom """
+    return type(obj) is not a_class
