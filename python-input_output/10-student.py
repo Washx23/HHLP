e884c734210ddf81fbf1e717a7e87cb385df5bc6
washx23 <5731@holbertonstudents.com>
2023-02-09 14:49:23 -0800

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" main """
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_list = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    new_list[i] = getattr(self, i)
+            return new_list
