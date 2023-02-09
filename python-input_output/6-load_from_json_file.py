f8b3f3f1e98f74ca37efdc01a70d85a781592e51
washx23 <5731@holbertonstudents.com>
2023-02-09 12:04:54 -0800

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+"""  main """
+
+import json
+
+
+def load_from_json_file(filename):
+    """ load_from_json func """
+    with open(filename, 'r') as f:
+        new_line = json.load(f)
+        return new_line
