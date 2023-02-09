bad5e09cb4c8862ab07b2d5f11652c8968ace8d4
washx23 <5731@holbertonstudents.com>
2023-02-09 09:40:51 -0800

    10-square.py

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Base Geometry """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", self.__size)
+
+    def area(self):
+        """area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
