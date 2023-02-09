0e795da3c35948d58962d404f5bd6e5552af2a5a
washx23 <5731@holbertonstudents.com>
2023-02-09 13:23:01 -0800

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,12 +7,19 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+""" main """
+
+
+
 filename = "add_item.json"
 args = sys.argv[1:]
+""" main """
+
 
 try:
     with open(filename, 'a+', encoding="utf-8") as f:
         my_list = load_from_json_file(filename)
+
 except FileNotFoundError:
     my_list = []
 my_list.extend(args)
