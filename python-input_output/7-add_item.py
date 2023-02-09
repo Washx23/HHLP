f91b052d53669b1a76a521e6aea74210f2bb09f9
washx23 <5731@holbertonstudents.com>
2023-02-09 13:16:44 -0800

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+args = sys.argv[1:]
+
+try:
+    with open(filename, 'a+', encoding="utf-8") as f:
+        my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+my_list.extend(args)
+save_to_json_file(my_list, filename)
