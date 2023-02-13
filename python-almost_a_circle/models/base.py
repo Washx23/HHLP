b31876d3f97bf6464ee6f0776dd798fe448e54ac
washx23 <5731@holbertonstudents.com>
2023-02-13 13:41:57 -0800

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """ first class """
 
 
+import json
+
+
 class Base:
     """ first base class """
 
@@ -13,3 +16,50 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """to_json_string"""
+        if list_dictionaries is None or list_dictionaries == []:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save_to_file"""
+        new_list = []
+        name = str(cls.__name__) + ".json"
+        with open(name, 'w', encoding='utf8') as f:
+            if list_objs is None:
+                f.write(cls.to_json_string(new_list))
+            else:
+                for obj in list_objs:
+                    new_list.append(cls.to_dictionary(obj))
+                f.write(cls.to_json_string(new_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """from_json_string"""
+        if json_string is None or json_string == []:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """create"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        file = cls.__name__ + ".json"
+        try:
+            with open(file, 'r') as f:
+                return [cls.create(**dictionary) for
+                        dictionary in cls.from_json_string(f.read())]
+        except FileNotFoundError:
+            return []
