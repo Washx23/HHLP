7f31fe56ac36a938711541b7a9708484bb6a0feb
washx23 <5731@holbertonstudents.com>
2023-03-17 10:28:41 -0700

    11-genre_id_all_shows.sql

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- import database 
+SELECT te_shows.title, tv_shows_genres.genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
