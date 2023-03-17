158b6fcce3a1d153ae1ba9ed033a5a321784d2e8
washx23 <5731@holbertonstudents.com>
2023-03-17 08:37:11 -0700

    2-create_read_user.sql

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create a user (READ USER)
+CREATE database IF NOT exists hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
