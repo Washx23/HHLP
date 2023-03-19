f9ef1d7b16b928cd06d4274d1f4042b2f37afece
washx23 <5731@holbertonstudents.com>
2023-03-19 15:48:39 -0700

    16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -4,4 +4,4 @@ FROM
     tv_shows
     LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BYtv_shows.title ASC, tv_genres.name ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
