65077537de7837529b79ca1de46655ebc579222a
washx23 <5731@holbertonstudents.com>
2023-03-19 15:53:15 -0700

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- Cretae a database whit a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT UNIQUE auto_increment,
-    name VARCHAR(256),
-    PRIMARY KEY(id)
-    );
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id));
