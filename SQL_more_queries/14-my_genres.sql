136cadfd76916812827350e540ca27ead60ef115
washx23 <5731@holbertonstudents.com>
2023-03-19 15:42:21 -0700

    14-my_genres.sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- import database 
 SELECT tv_genres.name AS name
-FROM 
+FROM
     tv_genres
     INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
     INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = "Dexter" 
+WHERE tv_shows.title = "Dexter"
 ORDER BY name ASC;
