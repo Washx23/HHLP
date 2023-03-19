6612918cb708852496f4648ead665dc63639946d
washx23 <5731@holbertonstudents.com>
2023-03-19 15:25:00 -0700

    10-genre_id_by_show.sql fix

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
--- Import the database
-
-SELECT tv_shows.title, tv_shows_genres.genre_id
-FROM tv_shows JOIN tv_show_genres 
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+-- import database 
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+    JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
