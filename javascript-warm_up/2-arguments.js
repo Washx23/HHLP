5004e87d32146ec040de58df7e347cdc14811d44
Washx23 <113697466+Washx23@users.noreply.github.com>
2023-04-13 10:35:22 -0300

    git not found

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
+
 const args = process.argv.length - 2;
-if (args >= 1) {
+if (args >= 2) {
+  console.log('Arguments found');
+} else if (args === 1) {
   console.log('Argument found');
 } else {
-  console.log('Argument not found');
+  console.log('No argument');
 }
